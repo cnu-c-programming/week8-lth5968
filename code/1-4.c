3296ce0d62a0fd89a39d1696af9f78bf362c4b30
lth5968 <lth5968@gmail.com>
2026-04-29 01:25:41 +0900

    Update 1-4.c

--- code/1-4.c
+++ code/1-4.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 
 int my_strcmp(const char *a, const char *b) {
-    while (*a != '\0' && *b != '\0') {
-        if (*a != *b) {
-            return (int)(*a - *b);
-        }
+    while (*a != '\0' && *a == *b) {
         a++;
         b++;
     }
-    return (int)(*a - *b);
+    
+    return *a - *b;
 }
 
 int main() {
-    printf("%d\n", my_strcmp("abc", "abd"));
+    printf("%d\n", my_strcmp("abc", "abcd"));
     printf("%d\n", my_strcmp("abc", "abc"));
     printf("%d\n", my_strcmp("b", "a"));
     return 0;
