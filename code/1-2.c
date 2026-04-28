dd5e39e7609041b2f1b549b05845151b8e3e0006
lth5968 <lth5968@gmail.com>
2026-04-29 00:56:48 +0900

    Update 1-2.c

--- code/1-2.c
+++ code/1-2.c
@@ -4,7 +4,7 @@
 int main() {
     char s1[10] = "hi";
     char s2[] = "hi";
-    char s3[] = {'h', 'i' '\0'};
+    char s3[] = {'h', 'i', '\0'};
 
     printf("%zu %zu\n", sizeof(s1), strlen(s1));
     printf("%zu %zu\n", sizeof(s2), strlen(s2));
